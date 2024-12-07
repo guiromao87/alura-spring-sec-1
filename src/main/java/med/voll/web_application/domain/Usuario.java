e15450bb8bfb6c4843ef6394fcb83be2ab9c5c38
Guilherme Romão <guilhermersimoes87@gmail.com>
2024-12-07 11:24:53 -0300

    create entity and migration for user

---
+++ src/main/java/med/voll/web_application/domain/Usuario.java
@@ -0,0 +1,15 @@
+package med.voll.web_application.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
